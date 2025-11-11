dd34522679fb58e7f79357edee5fba4ebf8e32d6
Yunhao <Firo.Y2h@gmail.com>
2025-11-11 16:54:59 +0200

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from pathlib import Path
 DEFAULT_INPUT = "/m/cs/scratch/sinitaivas/bluesky_firehose/firehose_stream/2024-12-17/2024-12-17T21.ndjson.gz"
 DEFAULT_SCRIPT = "/scratch/cs/ecanet/polwell-mental-health/polwell_mh/polwell_mh/preprocess/query_users_posts.py"
 DEFAULT_OUTPUT = str(Path("/tmp") / "test_user_posts.parquet")
-DEFAULT_N_USERS = 5
+DEFAULT_N_USERS = 10
 
 
 def pytest_addoption(parser):
